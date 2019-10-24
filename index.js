713a18515f6d5ae53af5c3fbdef91d1eac293d1e
Now-You-See-Me <37010054+Now-You-See-Me@users.noreply.github.com>
2019-10-24 11:46:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World master updated!");
+    response.end("Im starving but i only need RedBull!");
 });
 
 const port = process.env.PORT || 1337;
